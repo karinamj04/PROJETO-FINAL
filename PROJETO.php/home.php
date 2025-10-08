49f3a11cc730515b105a7cbfbb8a18f050332933
Arthur <133824941+chaesblue@users.noreply.github.com>
2025-10-08 19:51:41 -0300

    Update home.php
    atualizando pagina home para funcionar melhor com o darkmode e css

--- PROJETO.php/home.php
+++ PROJETO.php/home.php
@@ -4,24 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-    
-<style>
-  body {
-  margin: 0;
-  font-family: 'Nunito', sans-serif;
-  background-image: url('img/fundo1.jpg');
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: justify;  
-}
-</style>
-    
-
+    <script type="text/javascript" src="js/darkmode.js" defer></script>
     <title>HOME - Raízes da Saúde</title>
 </head>
 <body>
@@ -60,14 +45,14 @@
                 <li><a class="dropdown-item" href="quemsomos.php">Raio-x</a></li>
                 <li><a class="dropdown-item" href="quemsomos.php">Mapa</a></li>
                 <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="cadastro.php">Agende sua consulta</a></li>
+                <li><a class="dropdown-item" href="cadastro.html">Agende sua consulta</a></li>
               </ul>
             </li> 
           
             <li class="nav-item">
-              <a class="nav-link" href="sobrenos.php">Sobre Nós</a>
+              <a class="nav-link" href="sobrenos.html">Sobre Nós</a>
             </li>
-
+            <button id="ModoEscuro"><i class="bi bi-moon-fill"></i><i class="bi bi-brightness-high-fill"></i></button>
             </nav>
 
           <button id="cadastro">Cadastre-se</button>
@@ -75,29 +60,45 @@
           
     </header>
 
+ <div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/banner1.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/banner2.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/banner3.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
     <div class="texto-esquerda">
         <h1>Bem-vindo à Raízes da Saúde</h1>
         <p>Na Raízes da Saúde, cuidamos de cada paciente com carinho, responsabilidade e respeito.
-Nossa missão é oferecer um cuidado humanizado e acessível, promovendo a saúde e o bem-estar de todos.
-Com consultas a partir de R$90,00, nosso atendimento é simples, eficiente e realizado por profissionais qualificados.
+        Nossa missão é oferecer um cuidado humanizado e acessível, promovendo a saúde e o bem-estar de todos.
+        Com consultas a partir de R$90,00, nosso atendimento é simples, eficiente e realizado por profissionais qualificados.
 
-Valorizamos a escuta, o acolhimento e a confiança, garantindo que cada pessoa se sinta segura e bem cuidada desde o primeiro contato
+        Valorizamos a escuta, o acolhimento e a confiança, garantindo que cada pessoa se sinta segura e bem cuidada desde o primeiro contato
         </p>
-       <a href="contato.php" class="btn-contato">Nosso Contato</a>
+       <a href="contato.php" id="btn-Contato" class="btn-contato">Nosso Contato</a>
     
     </div>
     
-
-
-
-
-
-
-
-
-
-
 
 
 
